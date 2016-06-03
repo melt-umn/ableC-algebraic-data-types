d7aef955d85422fce13354ac123051cd65d082c9
Lucas Kramer <krame505@umn.edu>
2016-06-02 20:25:28 -0500

    Updated with current status of transparent prefixes

--- test/gc_and_default_datatype/artifact/Main.sv
+++ test/gc_and_default_datatype/artifact/Main.sv
@@ -2,19 +2,15 @@
 import edu:umn:cs:melt:ableC:concretesyntax as cst;
 import edu:umn:cs:melt:ableC:drivers:parseAndPrint;
 
-import edu:umn:cs:melt:exts:ableC:algDataTypes:datatype;
-import edu:umn:cs:melt:exts:ableC:algDataTypes:gcdatatype;
-
 -- TODO: Update this when transparent prefixes are done
-disambiguate edu:umn:cs:melt:exts:ableC:algDataTypes:datatype:concretesyntax:datatypeKeyword:Datatype_t, edu:umn:cs:melt:exts:ableC:algDataTypes:gcdatatype:datatypeKeyword:Datatype_t {
-  pluck edu:umn:cs:melt:exts:ableC:algDataTypes:datatype:concretesyntax:datatypeKeyword:Datatype_t;
-}
-
 parser extendedParser :: cst:Root {
   edu:umn:cs:melt:ableC:concretesyntax;
   edu:umn:cs:melt:exts:ableC:algDataTypes:datatype;
-  edu:umn:cs:melt:exts:ableC:algDataTypes:gcdatatype prefix edu:umn:cs:melt:exts:ableC:algDataTypes:gcdatatype:datatypeKeyword:Datatype_t with 'GC:';
+  edu:umn:cs:melt:exts:ableC:algDataTypes:gcdatatype prefix with 'GC:';
   edu:umn:cs:melt:exts:ableC:algDataTypes:patternmatching;
+  
+  prefer edu:umn:cs:melt:exts:ableC:algDataTypes:datatype:concretesyntax:datatypeKeyword:Datatype_t
+    over edu:umn:cs:melt:exts:ableC:algDataTypes:gcdatatype:datatypeKeyword:Datatype_t;
 } 
 
 function main
