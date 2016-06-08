2ef15e6fed07afdc0ec83a7e05a56b2a44a1b3ca
Eric Van Wyk <evw@cs.umn.edu>
2016-06-08 14:21:35 -0500

    added a  clause

--- src/abstractsyntax/PatternNotBoth.sv
+++ src/abstractsyntax/PatternNotBoth.sv
@@ -7,7 +7,11 @@ p::Pattern ::= p1::Pattern p2::Pattern
 
   p.defs = p1.defs ++ p2.defs;
   p1.env = p.env;
-  p2.env = p.env;
+
+  --ToDo - ensure pattern vars from p1 are visible in p2 for use
+  -- in 'when' pattern.  Or, rewrite 'when' to be
+  --  p::Pattern ::=  p1::Pattern 'when' e::Expr - which might be better anyway.
+  p2.env = addEnv(p1.defs, p.env);
 
   p1.expectedType = p.expectedType;
   p2.expectedType = p.expectedType;
@@ -15,6 +19,7 @@ p::Pattern ::= p1::Pattern p2::Pattern
   p.decls = p1.decls ++ p2.decls;
 
   p.errors := []; --ToDo
+
   p.transform = seqStmt (p1.transform, p2.transform);
 }
 
@@ -32,4 +37,27 @@ p::Pattern ::= p1::Pattern
   p.transform = seqStmt (p1.transform, flip_match);
   local flip_match :: Stmt = 
     txtStmt ("if (_match == 0) { _match = 1; } else { _match = 0; }");
+}
+
+
+
+abstract production patternWhen
+p::Pattern ::= e::Expr
+{
+  p.pp = text("when");
+  p.decls = [];
+  p.defs = [];
+  p.errors := [];
+  p.transform 
+    = ifStmt(
+        e,
+        -- txtExpr("( *_curr_scrutinee_ptr != " ++ show(10, constExpr.pp) ++ ")",
+        --        location=p.location),
+
+        -- then clause
+        nullStmt(),
+        -- else clause
+        txtStmt("_match = 0;")
+      );
+
 }
