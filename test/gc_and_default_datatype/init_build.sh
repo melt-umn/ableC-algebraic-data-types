d60242ffc4c520ee7b56848b229398eef29b2ed9
Lucas Kramer <krame505@umn.edu>
2016-06-17 21:03:47 -0500

    Fix build deps

--- test/gc_and_default_datatype/init_build.sh
+++ test/gc_and_default_datatype/init_build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-source $ABLEC_PATH/extensions/gc/init_build.sh
 source ../default/init_build.sh
+source ../gcdatatype/init_build.sh
 
-export ABLEC_SOURCE+=" $ADT_PATH/gcdatatype/**/*"
