5e8b0c4b2d62d3ffea3d2e858b98a7b91bba2dd2
Lucas Kramer <krame505@umn.edu>
2016-06-17 20:54:48 -0500

    Fixing options, no flow errors now

--- datatype/Export.sv
+++ datatype/Export.sv
@@ -3,12 +3,3 @@ grammar edu:umn:cs:melt:exts:ableC:algDataTypes:datatype;
 exports edu:umn:cs:melt:exts:ableC:algDataTypes:datatype:abstractsyntax;
 exports edu:umn:cs:melt:exts:ableC:algDataTypes:datatype:concretesyntax;
 exports edu:umn:cs:melt:exts:ableC:algDataTypes:datatype:mda_test;
-
--- These are essentially pure extensions to datatype, but we consider them as part of the same overall extension
--- so we include them to force a build every time
-option edu:umn:cs:melt:exts:ableC:algDataTypes:gcdatatype;
-option edu:umn:cs:melt:exts:ableC:algDataTypes:patternmatching;
-
--- These aren't pure extensions to datatype so we need to include them as options
-option edu:umn:cs:melt:exts:ableC:algDataTypes:rewrite;
-option edu:umn:cs:melt:exts:ableC:algDataTypes:deriving;
---
+++ datatype/abstractsyntax/Exports.sv
@@ -0,0 +1,5 @@
+grammar edu:umn:cs:melt:exts:ableC:algDataTypes:datatype:abstractsyntax;
+
+-- These aren't pure extensions to datatype so we need to include them as options
+option edu:umn:cs:melt:exts:ableC:algDataTypes:rewrite;
+option edu:umn:cs:melt:exts:ableC:algDataTypes:deriving;
