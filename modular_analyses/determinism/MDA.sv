529fd25e2087eaf180733e67a5a2ca6e962ddb23
Lucas Kramer <krame505@umn.edu>
2017-07-02 15:04:20 -0500

    Fix MDA.sv

--- modular_analyses/determinism/MDA.sv
+++ modular_analyses/determinism/MDA.sv
@@ -9,6 +9,5 @@ import edu:umn:cs:melt:ableC:host;
 
 copper_mda testStmt(ablecParser) {
   edu:umn:cs:melt:ableC:host;
-  edu:umn:cs:melt:exts:ableC:skeleton;
+  edu:umn:cs:melt:exts:ableC:algebraicDataTypes;
 }
-
