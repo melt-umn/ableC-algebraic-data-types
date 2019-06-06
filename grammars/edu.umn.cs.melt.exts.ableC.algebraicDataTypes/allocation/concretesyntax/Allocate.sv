1817aa65a7047578f4b2490d5893a5aa99d44e8e
Joe Blanchard <blanc317@umn.edu>
2019-06-06 12:19:48 -0500

    Make temporary change so that we don't need to use qualified names like malloc_Fork and can just use Fork. Eric says he wants this for the demo for sure, but maybe after as well?

--- grammars/edu.umn.cs.melt.exts.ableC.algebraicDataTypes/allocation/concretesyntax/Allocate.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.algebraicDataTypes/allocation/concretesyntax/Allocate.sv
@@ -12,6 +12,7 @@ marking terminal Allocate_t 'allocate' lexer classes {Ckeyword};
 terminal Datatype_t 'datatype';
 terminal With_t 'with';
 
+{--
 concrete production allocateDecl_c
 -- id is Identifer_t here to avoid follow spillage
 top::Declaration_c ::= 'allocate' 'datatype' id::Identifier_t 'with' alloc::Identifier_c ';'
@@ -23,6 +24,23 @@ action {
       addIdentsToScope(
         map(
           \ c::String -> name(alloc.ast.name ++ "_" ++ c, location=id.location),
+          constructors.fromJust),
+        Identifier_t,
+        context);
+  -- If the datatype hasn't been declared, then do nothing
+}
+--}
+
+concrete production allocateDeclShortName_c
+top::Declaration_c ::= 'allocate' 'datatype' id::Identifier_t 'with' alloc::Identifier_c ';'
+{ top.ast = allocateDecl(fromId(id), alloc.ast); }
+action {
+  local constructors::Maybe<[String]> = lookupBy(stringEq, id.lexeme, adtConstructors);
+  if (constructors.isJust)
+    context =
+      addIdentsToScope(
+        map(
+          \ c::String -> name(c, location=id.location),
           constructors.fromJust),
         Identifier_t,
         context);
