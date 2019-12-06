d2784bb1a9fc320382721093ae02fbe97736d4d2
Lucas Kramer <krame505@umn.edu>
2019-12-06 15:28:30 -0600

    Use Identiifer_c for datatype forward declaration

--- grammars/edu.umn.cs.melt.exts.ableC.algebraicDataTypes/datatype/concretesyntax/DatatypeFwd.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.algebraicDataTypes/datatype/concretesyntax/DatatypeFwd.sv
@@ -2,10 +2,7 @@ grammar edu:umn:cs:melt:exts:ableC:algebraicDataTypes:datatype:concretesyntax;
 
 -- e.g. "datatype Type;"
 -- Forward declaration, mirroring C stucts closely
-concrete productions s::StructOrUnionSpecifier_c
-| 'datatype' id::Identifier_t
-    { s.realTypeSpecifiers = 
-        [ adtTagReferenceTypeExpr( s.givenQualifiers, 
-             name(id.lexeme, location=s.location) ) ];
-    }
+concrete productions top::StructOrUnionSpecifier_c
+| 'datatype' id::Identifier_c
+    { top.realTypeSpecifiers = [adtTagReferenceTypeExpr(top.givenQualifiers, id.ast)]; }
 
