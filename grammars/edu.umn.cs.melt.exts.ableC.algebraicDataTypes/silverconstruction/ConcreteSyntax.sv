d65b4c8ed0b1b176aed2a1cf8277a1e038cc0e44
Lucas Kramer <krame505@umn.edu>
2020-02-14 19:57:50 -0600

    Add MDA test for silver construction grammar

--- grammars/edu.umn.cs.melt.exts.ableC.algebraicDataTypes/silverconstruction/ConcreteSyntax.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.algebraicDataTypes/silverconstruction/ConcreteSyntax.sv
@@ -2,15 +2,15 @@ grammar edu:umn:cs:melt:exts:ableC:algebraicDataTypes:silverconstruction;
 
 imports edu:umn:cs:melt:exts:silver:ableC:concretesyntax;
 
-exports edu:umn:cs:melt:exts:ableC:algebraicDataTypes:datatype;
-exports edu:umn:cs:melt:exts:ableC:algebraicDataTypes:patternmatching;
-exports edu:umn:cs:melt:exts:ableC:algebraicDataTypes:allocation;
+imports edu:umn:cs:melt:exts:ableC:algebraicDataTypes:datatype;
+imports edu:umn:cs:melt:exts:ableC:algebraicDataTypes:patternmatching;
+imports edu:umn:cs:melt:exts:ableC:algebraicDataTypes:allocation;
 
-terminal EscapeConstructorList_t '$ConstructorList' lexer classes {Escape, Reserved};
-terminal EscapeStmtClauses_t     '$StmtClauses'     lexer classes {Escape, Reserved};
-terminal EscapeExprClauses_t     '$ExprClauses'     lexer classes {Escape, Reserved};
-terminal EscapePatternList_t     '$PatternList'     lexer classes {Escape, Reserved};
-terminal EscapePattern_t         '$Pattern'         lexer classes {Escape, Reserved};
+marking terminal AntiquoteConstructorList_t '$ConstructorList' lexer classes {Antiquote, Reserved};
+marking terminal AntiquoteStmtClauses_t     '$StmtClauses'     lexer classes {Antiquote, Reserved};
+marking terminal AntiquoteExprClauses_t     '$ExprClauses'     lexer classes {Antiquote, Reserved};
+marking terminal AntiquotePatternList_t     '$PatternList'     lexer classes {Antiquote, Reserved};
+marking terminal AntiquotePattern_t         '$Pattern'         lexer classes {Antiquote, Reserved};
 
 concrete productions top::Constructor_c
 | '$ConstructorList' silver:definition:core:LCurly_t e::Expr silver:definition:core:RCurly_t
